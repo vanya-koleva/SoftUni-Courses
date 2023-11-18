96df2d15135dfabefe01ca438d474de51c40ed2a
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2023-11-18 22:03:21 +0200

    Deleted files used for reviewing

--- Programming_Basics/nested_loops/review/multiplication_table.py
+++
@@ -1,4 +0,0 @@
-for first_multiplier in range(1, 10 + 1):
-    for second_multiplier in range(1, 10 +1):
-        result = first_multiplier * second_multiplier
-        print(f"{first_multiplier} * {second_multiplier} = {result}")
