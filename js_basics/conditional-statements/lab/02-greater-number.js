2190daa228c4023cf2a02fa545a03785b33c769e
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-05 03:18:33 +0300

    Add a solution

---
+++ js_basics/conditional-statements/lab/02-greater-number.js
@@ -0,0 +1,10 @@
+function greaterNumber(input) {
+    let num1 = input[0];
+    let num2 = input[1];
+    if (num1 > num2)
+        console.log(num1);
+    else
+        console.log(num2);
+}
+
+greaterNumber(["10", "10"])
