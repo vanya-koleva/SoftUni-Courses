e55ce6fbfc0697d010311e3c296e3200c4ea8f0b
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-07 19:29:09 +0300

    Add a solution

---
+++ js_basics/conditional-statements/exercise/02-bonus-score.js
@@ -0,0 +1,20 @@
+function calculateScore(input) {
+    let num = Number(input[0]);
+    let bonus = 0.0;
+    if (num <= 100) {
+        bonus = 5;
+    } else if (num > 1000) {
+        bonus = num * 0.1;
+    } else {
+        bonus = num * 0.2;
+    }
+    if (num % 2 == 0) {
+        bonus += 1
+    } else if (num % 5 == 0) {
+        bonus += 2
+    }
+    console.log(bonus);
+    console.log(num + bonus);
+}
+
+calculateScore(["2703"])
