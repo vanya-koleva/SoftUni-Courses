53374906f281af541f9199ca60ed3e887389ee42
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-09 20:51:50 +0300

    Add a solution

---
+++ js_basics/conditional-statements-advanced/lab/04-personal-titles.js
@@ -0,0 +1,21 @@
+function getTitle(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if (gender == "m") {
+        if (age >= 16) {
+            console.log("Mr.")
+        } else {
+            console.log("Master")
+        }
+    } else {
+        if (age >= 16) {
+            console.log("Ms.")
+        } else {
+            console.log("Miss")
+        }
+    }
+}
+
+getTitle(["13.5", "m"])
+
