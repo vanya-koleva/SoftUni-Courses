6876f50f52ae13fb910f8dfe1f3aa2fa66b9e69c
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-09 20:37:48 +0300

    Add a solution

---
+++ js_basics/conditional-statements-advanced/lab/03-animal-type.js
@@ -0,0 +1,18 @@
+function getAnimalType(input) {
+    let animal = input[0];
+    switch (animal) {
+        case "dog":
+            console.log("mammal");
+            break;
+        case "crocodile":
+        case "tortoise":
+        case "snake":
+            console.log("reptile");
+            break;
+        default:
+            console.log("unknown");
+            break;
+    }
+}
+
+getAnimalType(["snake"])
