0aefd8b4ec0b7b953debac87dc54765d0ec18179
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-12 02:19:39 +0300

    Add a solution

---
+++ js_basics/conditional-statements-advanced/lab/08-cinema-ticket.js
@@ -0,0 +1,15 @@
+function ticketPrice(input) {
+    let day = input[0];
+    let price = 0;
+
+    if (day == "Monday" || day == "Tuesday" || day == "Friday") {
+        price = 12
+    } else if (day == "Wednesday" || day == "Thursday") {
+        price = 14
+    } else {
+        price = 16
+    }
+    console.log(price)
+}
+
+ticketPrice(["Sunday"])
