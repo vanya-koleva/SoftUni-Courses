02487e1e93e1665a2871d48fbdaf3caa8fbeb51d
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-11 10:23:44 +0300

    Add a solution

---
+++ js_basics/conditional-statements-advanced/lab/06-number-in-range.js
@@ -0,0 +1,10 @@
+function numInRange(input) {
+    let num = Number(input[0]);
+    if (num >= -100 && num <= 100 && num != 0) {
+        console.log("Yes")
+    } else {
+        console.log("No")
+    }
+}
+
+numInRange(["0"])
