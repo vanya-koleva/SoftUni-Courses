32cbca1540f339725882d741feb8b8efa4990252
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-06-27 10:21:04 +0300

    Add a solution

---
+++ python_advanced/file_handling/exercise/01_even_lines.py
@@ -0,0 +1,14 @@
+import os
+
+
+symbols = ("-", ",", ".", "!", "?")
+path = os.path.join("text_files", "01_text.txt")
+
+with open(path) as file:
+    text = file.readlines()
+
+for line in range(0, len(text), 2):
+    for symbol in symbols:
+        text[line] = text[line].replace(symbol, "@")
+
+    print(*text[line].split()[::-1])
