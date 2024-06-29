3ac68d6b89b9cc8e81a4c4f96d3da01fe7de6b3b
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-06-29 20:27:57 +0300

    Add a solution

---
+++ python_oop/02_classes_and_objects/exercise/01_vet_02.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class Vet:
+    animals: List[str] = []
+    space: int = 5   # SPACE = 5
+
+    def __init__(self, name: str):
+        self.name = name
+        self.animals: List[str] = []
+
+    def register_animal(self, animal_name: str) -> str:
+        if len(Vet.animals) < Vet.space:
+            Vet.animals.append(animal_name)
+            self.animals.append(animal_name)
+            return f"{animal_name} registered in the clinic"
+
+        return "Not enough space"
+
+    def unregister_animal(self, animal_name) -> str:
+        if animal_name in self.animals:
+            self.animals.remove(animal_name)
+            Vet.animals.remove(animal_name)
+            return f"{animal_name} unregistered successfully"
+
+        return f"{animal_name} not in the clinic"
+
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals." \
+               f" {Vet.space - len(Vet.animals)} space left in clinic"
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
