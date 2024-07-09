ae34b84ff39094f702c8896da8c7375b8b2c22c9
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-07-09 11:51:27 +0300

    Add a solution

---
+++ python_oop/05_class_and_static_methods/lab/04_hotel_rooms/project/hotel.py
@@ -0,0 +1,38 @@
+from typing import List
+from project.room import Room
+
+
+class Hotel:
+    def __init__(self, name: str):
+        self.name = name
+        self.rooms: List[Room] = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room: Room) -> None:
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people) -> None:
+        room = next(filter(lambda r: r.number == room_number, self.rooms), None)
+        if room:
+            result = room.take_room(people)
+
+            if not result:
+                self.guests += people
+
+    def free_room(self, room_number) -> None:
+        room = next(filter(lambda r: r.number == room_number, self.rooms), None)
+        if room:
+            people = room.guests
+            result = room.free_room()
+
+            if not result:
+                self.guests -= people
+
+    def status(self) -> str:
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '.join(str(r.number) for r in self.rooms if not r.is_taken)}\n" \
+               f"Taken rooms: {', '.join(str(r.number) for r in self.rooms if r.is_taken)}"
