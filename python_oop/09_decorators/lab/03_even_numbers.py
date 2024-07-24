473e0f083949aec407c7ed99faf0b51322a9dc73
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-07-24 18:53:44 +0300

    Add a solution

---
+++ python_oop/09_decorators/lab/03_even_numbers.py
@@ -0,0 +1,15 @@
+def even_numbers(function):
+
+    def wrapper(numbers):
+
+        return [number for number in function(numbers) if number % 2 == 0]
+
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
