30ed1f833091e36d04e7fbb47ca611d260ce300e
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-07-25 19:29:54 +0300

    Add a solution

---
+++ python_oop/09_decorators/exercise/02_even_parameters.py
@@ -0,0 +1,21 @@
+def even_parameters(func):
+
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if isinstance(arg, int):
+                if arg % 2 == 0:
+                    continue
+            return "Please use only even numbers!"
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
