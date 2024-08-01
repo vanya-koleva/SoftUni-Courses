3e9d7451b4a9f21e5319d31b1e937624a392659e
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-08-01 21:49:22 +0300

    Add a solution

---
+++ python_oop/exam_06/tasks_1_2/project/clients/adult.py
@@ -0,0 +1,15 @@
+from project.clients.base_client import BaseClient
+
+
+class Adult(BaseClient):
+    INITIAL_INTEREST = 4.0
+
+    def __init__(self, name: str, client_id: str, income: float):
+        super().__init__(name, client_id, income, self.INITIAL_INTEREST)
+
+    def increase_clients_interest(self):
+        self.interest += 2.0
+
+    @staticmethod
+    def type_of_loan():
+        return "MortgageLoan"
