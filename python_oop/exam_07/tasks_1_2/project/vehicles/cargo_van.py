9a46ee4ccdafffc19ddc75990a374495e44bf179
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-08-03 21:49:07 +0300

    Add a solution

---
+++ python_oop/exam_07/tasks_1_2/project/vehicles/cargo_van.py
@@ -0,0 +1,12 @@
+from project.vehicles.base_vehicle import BaseVehicle
+
+
+class CargoVan(BaseVehicle):
+    MAX_MILEAGE = 180.00
+
+    def __init__(self, brand: str, model: str, license_plate_number: str):
+        super().__init__(brand, model, license_plate_number, self.MAX_MILEAGE)
+
+    def drive(self, mileage: float):
+        reduce_amount = mileage / self.MAX_MILEAGE * 100
+        self.battery_level -= (round(reduce_amount) + 5)
