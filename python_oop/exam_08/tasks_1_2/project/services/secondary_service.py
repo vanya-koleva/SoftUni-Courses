6ec082b71d7fd573d46a367e74bdc46f3a7ebe50
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-08-05 21:34:09 +0300

    Add a solution

---
+++ python_oop/exam_08/tasks_1_2/project/services/secondary_service.py
@@ -0,0 +1,12 @@
+from project.services.base_service import BaseService
+
+
+class SecondaryService(BaseService):
+    CAPACITY = 15
+
+    def __init__(self, name: str):
+        super().__init__(name, self.CAPACITY)
+
+    @property
+    def service_type(self):
+        return "Secondary"
