4122c935ee9670880f340b9c92d8c2701286171d
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-08-06 22:02:12 +0300

    Add a solution

---
+++ python_oop/exam_09/tasks_1_2/project/band_members/singer.py
@@ -0,0 +1,11 @@
+from project.band_members.musician import Musician
+
+
+class Singer(Musician):
+
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+
+    @property
+    def available_skills(self):
+        return ["sing high pitch notes", "sing low pitch notes"]
