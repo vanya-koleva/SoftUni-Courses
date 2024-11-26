98d3636ebf3fd9801a19537fbfd93a2a8f9d186b
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-11-26 10:01:09 +0200

    Add a solution

---
+++ js_front_end/01-fundamentals/lab/02-student-information.js
@@ -0,0 +1,6 @@
+function solve(name, age, avgGrade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`);
+}
+solve('John', 15, 5.54678);
+solve('Steve', 16, 2.1426);
+solve('Marry', 12, 6.0);
