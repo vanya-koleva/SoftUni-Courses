eae96d07d2c3eb2bb71ac0ec75d02c0172c09119
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-11-28 22:00:33 +0200

    Add a solution

---
+++ js_front_end/02-arrays-and-strings/lab/01-sum-first-and-last-array-elements.js
@@ -0,0 +1,9 @@
+function solve(input) {
+    const first = input[0];
+    const last = input[input.length - 1];
+    console.log(first + last);
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
