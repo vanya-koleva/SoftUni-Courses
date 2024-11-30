8af4914e7a1cf2c3c964b8c22c979699fd5a06e4
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-11-30 18:54:39 +0200

    Add a solution

---
+++ js_front_end/03-functions-and-statements/lab/02-math-power.js
@@ -0,0 +1,12 @@
+function solve(num, power) {
+    let result = 1;
+
+    for (let i = 0; i < power; i++) {
+        result *= num;
+    }
+
+    console.log(result);
+}
+
+solve(2, 8);
+solve(3, 4);
