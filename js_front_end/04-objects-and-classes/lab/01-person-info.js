9c78e9ade5f1d06bb79e960728751e8cbe52c931
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-12-01 23:01:20 +0200

    Add a solution

---
+++ js_front_end/04-objects-and-classes/lab/01-person-info.js
@@ -0,0 +1,11 @@
+function solve(firstName, lastName, age) {
+    const info = {
+        firstName,
+        lastName,
+        age,
+    };
+
+    return info;
+}
+
+console.log(solve('Peter', 'Pan', '20'));
