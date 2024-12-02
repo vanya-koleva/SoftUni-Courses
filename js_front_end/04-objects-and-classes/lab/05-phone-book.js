c39091f2da2cedbd0d517f3bbf81fec033293588
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-12-02 19:22:37 +0200

    Add a solution

---
+++ js_front_end/04-objects-and-classes/lab/05-phone-book.js
@@ -0,0 +1,14 @@
+function solve(input) {
+    let phoneBook = {};
+
+    for (const entry of input) {
+        let [name, phone] = entry.split(' ');
+        phoneBook[name] = phone;
+    }
+
+    for (const name in phoneBook) {
+        console.log(`${name} -> ${phoneBook[name]}`);
+    }
+}
+
+solve(['Tim 0834212554', 'Peter 0877547887', 'Bill 0896543112', 'Tim 0876566344']);
