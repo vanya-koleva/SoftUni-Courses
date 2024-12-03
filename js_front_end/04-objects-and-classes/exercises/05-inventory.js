ef2ba49d288ec25c7e704b4d20e1726e804ab144
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-12-03 22:58:45 +0200

    Add a solution

---
+++ js_front_end/04-objects-and-classes/exercises/05-inventory.js
@@ -0,0 +1,21 @@
+function solve(input) {
+    input
+        .map(raw => raw.split(' / '))
+        .map(([name, level, items]) => ({
+            name,
+            items,
+            level: Number(level),
+        }))
+        .sort((a, b) => a.level - b.level)
+        .forEach(hero => {
+            console.log(`Hero: ${hero.name}`);
+            console.log(`level => ${hero.level}`);
+            console.log(`items => ${hero.items}`);
+        });
+}
+
+solve([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara',
+]);
