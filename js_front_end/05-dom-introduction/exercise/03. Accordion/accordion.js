00eb8b04098960d6b54a30625e66dcdaa515153c
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-12-07 18:21:02 +0200

    Add a solution

---
+++ js_front_end/05-dom-introduction/exercise/03. Accordion/accordion.js
@@ -0,0 +1,12 @@
+function toggle() {
+    const buttonEl = document.querySelector('#accordion .button');
+    const contentEl = document.querySelector('#extra');
+
+    if (contentEl.style.display == 'block') {
+        contentEl.style.display = 'none';
+        buttonEl.textContent = 'More';
+    } else {
+        contentEl.style.display = 'block';
+        buttonEl.textContent = 'Less';
+    }
+}
