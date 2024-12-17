fbbb32f610814dda1bfa0c53adba78f3c29926b5
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-12-17 19:59:21 +0200

    Adding a solution

---
+++ js_front_end/06-dom-manipulation-and-events/exercise/01. Sections/sections.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', solve);
+
+function solve() {
+    const formEl = document.querySelector('#task-input');
+    const contentEl = document.querySelector('#content');
+
+    formEl.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const sections = formEl.querySelector('input[type="text"]').value.split(', ');
+
+        sections.forEach(el => {
+            const newDivEl = document.createElement('div');
+            const newPEl = document.createElement('p');
+
+            newPEl.textContent = el;
+            newPEl.style.display = 'none';
+
+            newDivEl.append(newPEl);
+            newDivEl.addEventListener('click', e => {
+                console.log(e.target);
+                e.target.querySelector('p').style.display = 'block';
+            });
+            contentEl.append(newDivEl);
+        });
+    });
+}
