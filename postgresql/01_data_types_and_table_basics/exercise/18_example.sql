496d87957be1c1e08045073ec79753b471d00b4c
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-01-10 02:20:48 +0200

    Adding a solution

---
+++ postgresql/01_data_types_and_table_basics/exercise/18_example.sql
@@ -0,0 +1,24 @@
+CREATE TYPE address AS (
+	street VARCHAR(50),
+	city VARCHAR(50),
+	postal_code CHAR(4)
+);
+
+CREATE TABLE customers (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	customer_name VARCHAR(100),
+	customer_address address
+);
+
+INSERT INTO
+	customers(customer_name, customer_address)
+VALUES
+	('Gosho', ('ulitsa Stara Planina', 'Sofia', 1600))
+;
+
+SELECT
+	(customer_address).street,
+	(customer_address).postal_code
+FROM
+	customers
+;
