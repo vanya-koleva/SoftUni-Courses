052543f5fe2b4e24d6119f49353d2bbc9fe643ca
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-01-19 18:25:15 +0200

    Adding a solution

---
+++ postgresql/04_data_aggregation/lab/08_department_names.sql
@@ -0,0 +1,17 @@
+SELECT
+	id,
+	first_name,
+	last_name,
+	TRUNC(salary, 2),
+	department_id,
+	CASE 
+		WHEN department_id = 1 THEN 'Management'
+		WHEN department_id = 2 THEN 'Kitchen Staff'
+		WHEN department_id = 3 THEN 'Service Staff'
+	ELSE 'Other'
+	END AS department_name
+FROM
+	employees
+ORDER BY
+	id
+;
