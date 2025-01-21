a3a08e2ae86431a389710e0d01a1e9d611febc3f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-01-21 03:08:32 +0200

    Add a solution

---
+++ postgresql/04_data_aggregation/exercise/10_wizard_view.sql
@@ -0,0 +1,20 @@
+CREATE VIEW 
+    view_wizard_deposits_with_expiration_date_before_1983_08_17
+AS
+SELECT
+	first_name || ' ' || last_name AS wizard_name,
+	deposit_start_date AS start_date,
+	deposit_expiration_date AS expiration_date,
+	deposit_amount AS amount
+FROM
+	wizard_deposits
+GROUP BY
+	wizard_name,
+	start_date,
+	expiration_date,
+	amount
+HAVING
+	deposit_expiration_date <= '1983-08-17'
+ORDER BY
+	expiration_date
+;
