3e5878dc4f9356fa6ab33bd978026e64204609a2
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-01-29 05:39:14 +0200

    Add a solution

---
+++ postgresql/07_data_programmability_and_transactions/lab/01_count_employees_by_town.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION 
+	fn_count_employees_by_town(town_name VARCHAR(20))
+RETURNS INT
+AS
+$$
+	DECLARE
+		count_employees INT;
+	BEGIN
+		SELECT 
+			COUNT(*) INTO count_employees
+		FROM
+			employees AS e
+		JOIN
+			addresses AS a
+		USING
+			(address_id)
+		JOIN
+			towns AS t
+		USING
+			(town_id)
+		WHERE 
+            t.name = town_name;
+		RETURN 
+            count_employees;
+	END
+$$
+LANGUAGE plpgsql;
+
+-- SELECT fn_count_employees_by_town('Sofia');
+
