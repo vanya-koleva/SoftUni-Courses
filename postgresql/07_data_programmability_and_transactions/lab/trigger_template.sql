de8e463bb2fe737610413a2dff80bf74d0b6f7ea
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-01-30 09:18:22 +0200

    Add a solution

---
+++ postgresql/07_data_programmability_and_transactions/lab/trigger_template.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION trigger_fn_function_name()
+RETURNS TRIGGER
+AS
+$$
+BEGIN
+	RETURN NULL; -- NULL/OLD/NEW
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_trigger_name
+AFTER DELETE 	--BEFORE/AFTER INSERT/UPDATE/DELETE
+ON the_table_name
+FOR EACH ROW 	-- FOR EACH ROW / FOR EACH STATEMENT
+EXECUTE PROCEDURE 	-- EXECUTE FUNCTION works in later PostgreSQL versions
+trigger_fn_function_name();
