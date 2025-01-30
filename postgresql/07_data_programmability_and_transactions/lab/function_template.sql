3eaee8e8f1a9006686ab78a3eafc50f69be195cd
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-01-30 09:00:43 +0200

    Add a solution

---
+++ postgresql/07_data_programmability_and_transactions/lab/function_template.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION fn_function_name(param_name param_type, param_2 type_2)
+RETURNS value_type AS
+$$
+	DECLARE
+		variable_name variable_type;
+		variable_2 type_2;
+	BEGIN
+        RETURN
+	END
+$$
+LANGUAGE plpgsql;
+
+SELECT fn_function_name(arg_1, arg_2);
