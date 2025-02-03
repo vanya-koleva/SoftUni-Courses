c017e636608283587502956b28fae10584a05dc1
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-03 05:58:00 +0200

    Add a solution

---
+++ postgresql/08_exam_preparation/01_database_design.sql
@@ -0,0 +1,43 @@
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    address VARCHAR(50)
+);
+
+CREATE TABLE animal_types(
+    id SERIAL PRIMARY KEY,
+    animal_type VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE cages(
+    id SERIAL PRIMARY KEY,
+    animal_type_id INT REFERENCES animal_types ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    birthdate DATE NOT NULL,
+    owner_id INT REFERENCES owners ON DELETE CASCADE ON UPDATE CASCADE,
+    animal_type_id INT REFERENCES animal_types ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+
+CREATE TABLE volunteers_departments(
+    id SERIAL PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE volunteers(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    address VARCHAR(50),
+    animal_id INT REFERENCES animals ON DELETE CASCADE ON UPDATE CASCADE,
+    department_id INT REFERENCES volunteers_departments ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+
+CREATE TABLE animals_cages(
+    cage_id INT REFERENCES cages ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    animal_id INT REFERENCES animals ON DELETE CASCADE  ON UPDATE CASCADE NOT NULL
+);
