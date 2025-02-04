77b90bbcb3f4d97e44f191131b0f882e242edaf9
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-04 06:30:18 +0200

    Add a solution

---
+++ postgresql/09_exam_preparation_2/01_database_design.sql
@@ -0,0 +1,47 @@
+CREATE TABLE addresses(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE clients(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE drivers(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    age INT NOT NULL CHECK ( age > 0 ),
+    rating NUMERIC DEFAULT 5.5
+);
+
+CREATE TABLE cars(
+    id SERIAL PRIMARY KEY,
+    make VARCHAR(20) NOT NULL,
+    model VARCHAR(20),
+    year INT DEFAULT 1 NOT NULL CHECK ( year > 0 ),
+    mileage INT DEFAULT 1 CHECK ( mileage > 0 ),
+    condition CHAR(1) NOT NULL,
+    category_id INT REFERENCES categories ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+
+CREATE TABLE courses(
+    id SERIAL PRIMARY KEY,
+    from_address_id INT REFERENCES addresses ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    start TIMESTAMP NOT NULL,
+    bill NUMERIC(10, 2) DEFAULT 10 CHECK ( bill > 0 ),
+    car_id INT REFERENCES cars ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    client_id INT REFERENCES clients ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+
+CREATE TABLE cars_drivers(
+    car_id INT REFERENCES cars ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    driver_id INT REFERENCES drivers ON DELETE CASCADE ON UPDATE CASCADE NOT NULL 
+);
