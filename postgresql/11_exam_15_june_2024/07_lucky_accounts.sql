f3895111326745f9f5d71b6a2b4012b158769cf2
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-07 09:28:08 +0200

    Add a solution

---
+++ postgresql/11_exam_15_june_2024/07_lucky_accounts.sql
@@ -0,0 +1,12 @@
+SELECT
+    CONCAT(a.id, ' ', a.username) AS id_username,
+    a.email
+FROM
+    accounts AS a
+JOIN
+    accounts_photos AS ap
+ON
+    a.id = ap.account_id
+WHERE
+    a.id = ap.photo_id
+;
