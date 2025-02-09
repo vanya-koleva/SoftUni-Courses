04e507d9037cfe2be35b482c9de05033e5a89e65
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-09 10:17:01 +0200

    Add a solution

---
+++ postgresql/13_exam_04_december_2023/06_negative_feedback.sql
@@ -0,0 +1,22 @@
+SELECT
+    f.product_id,
+    f.rate,
+    f.description,
+    f.customer_id,
+    c.age,
+    c.gender
+FROM
+    feedbacks AS f
+JOIN
+    customers AS c
+ON
+    f.customer_id = c.id
+WHERE
+    f.rate < 5
+        AND
+    c.gender = 'F'
+        AND
+    c.age > 30
+ORDER BY
+    product_id DESC
+;
