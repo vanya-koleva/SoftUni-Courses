36f8caaeb2b218c139279eed00d72ea8f8c2f794
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-19 16:07:30 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Lab/08.PetShop/Program.cs
@@ -0,0 +1,8 @@
+﻿int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+
+double dogPrice = dogFood * 2.50;
+int catPrice = catFood * 4;
+double totalPrice = dogPrice + catPrice;
+
+Console.WriteLine($"{totalPrice} lv.");
