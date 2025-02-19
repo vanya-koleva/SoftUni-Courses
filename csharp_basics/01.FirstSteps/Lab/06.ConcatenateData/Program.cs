c312cd8c5ff41b4516ef90a1b94c4b7f8112064f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-19 15:01:10 +0200

    Delete csharp_basics/01.FirstSteps/Lab/06.ConcatenateData directory

--- csharp_basics/01.FirstSteps/Lab/06.ConcatenateData/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿string firstName = Console.ReadLine();
-string lastName = Console.ReadLine();
-int age = int.Parse(Console.ReadLine());
-string city = Console.ReadLine();
-
-Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {city}.");
