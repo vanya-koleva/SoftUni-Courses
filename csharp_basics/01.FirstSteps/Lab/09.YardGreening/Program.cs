534cc7e9cb3340bbd7790030c0874bd47f29b1fb
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-19 16:21:27 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Lab/09.YardGreening/Program.cs
@@ -0,0 +1,8 @@
+﻿double squareMeters = double.Parse(Console.ReadLine());
+double pricePerSquareMeter = 7.61;
+
+double price = squareMeters * pricePerSquareMeter;
+double discount = price * 0.18;
+
+Console.WriteLine($"The final price is: {price - discount} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
