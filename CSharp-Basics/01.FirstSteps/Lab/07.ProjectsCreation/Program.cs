c5226167398f4cb75accf1bbde0537fe213a87b1
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-19 15:24:54 +0200

    Add a solution

---
+++ CSharp-Basics/01.FirstSteps/Lab/07.ProjectsCreation/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = Console.ReadLine();
+int numberOfProjects = int.Parse(Console.ReadLine());
+int hoursPerProject = 3;
+int hours = numberOfProjects * hoursPerProject;
+
+Console.WriteLine($"The architect {name} will need {hours} hours to complete {numberOfProjects} project/s.");
